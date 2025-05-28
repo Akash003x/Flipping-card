3b8bdfe4ffeb69008a028a43938301867091ed53
Akash003x <akash003x@gmail.com>
2025-05-29 01:43:55 +0530

    image location changed

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
             {/* Front Side */}
             <div className="absolute w-full h-full bg-gradient-to-br from-gray-900 via-gray-800 to-gray-700 rounded-3xl overflow-hidden backface-hidden duration-500 shadow-xl">
               <img
-                src="src/images/p2.jpg"
+                src="public/images/p2.jpg"
                 className="w-full h-full object-cover opacity-80"
                 alt="Batman"
               />
